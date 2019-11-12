201dc876f0971f97d5ebe73486a081715f626472
Sadneur <il.sadreev@gmail.com>
2019-11-12 14:28:42 +0300

    fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,4 +2,4 @@
 
 import userName from '..';
 
-export default userName;
+console.log(`Hello, ${userName}!`);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 console.log();
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
 
 export default userName;
